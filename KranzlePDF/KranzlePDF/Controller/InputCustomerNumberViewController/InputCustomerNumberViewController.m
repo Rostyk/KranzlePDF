54c8930eb69c7c9399b031d5fd5c7f11a74da9a4
Rostyslav Stepanyak <ros.aphex@gmail.com>
2015-01-13 12:05:08 +0200

    add incorrect customer number error handling

--- KranzlePDF/KranzlePDF/Controller/InputCustomerNumberViewController/InputCustomerNumberViewController.m
+++ KranzlePDF/KranzlePDF/Controller/InputCustomerNumberViewController/InputCustomerNumberViewController.m
@@ -31,17 +31,41 @@
 #pragma mark parse button clicked
 
 - (IBAction)parseButtonClicked:(id)sender {
+    //dismiss the keyboard
+    [self.customerNumberTextField resignFirstResponder];
+    
     NSString *customerNumber = self.customerNumberTextField.text;
     NSArray *customerRecord = [[DataProvider sharedProvider] recordForCustomerNumber:customerNumber];
+    if(!customerRecord) {
+        [self alert:[ NSString stringWithFormat:@"Customer with number %@ not found in our database", customerNumber]];
+        return;
+    }
+    
     Customer *customer = [[Customer alloc] initWithRecord: customerRecord];
+    //setup the renderer to display customer info
     [[PDFRenderer sharedRenderer] insertCustomerData: customer];
     [[PDFRenderer sharedRenderer] render];
     
     
+    [self pushPDFViewController];
+}
+
+
+#pragma mark show pdf view controller
+
+- (void)pushPDFViewController {
     UIStoryboard *mainStoryboard = [UIStoryboard storyboardWithName:@"Main" bundle: nil];
     PdfViewController *controller = (PdfViewController*)[mainStoryboard instantiateViewControllerWithIdentifier:@"PdfViewControllerID"];
     [self.navigationController pushViewController: controller animated:YES];
 }
 
+- (void)alert:(NSString *)text {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@""
+                                                    message:text
+                                                   delegate:self
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil];
+    [alert show];
+}
 
 @end
